7f3c70b3174f9ab260f3106ffdab6b2822ee2ce7
EdersonRodrigues7 <ederson@trilhante.com.br>
2021-10-09 14:54:40 -0300

    route create room, room controller, sqlite install

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,18 +2,41 @@ import Modal from './modal.js';
 
 const modal = Modal();
 
+const modalTitle = document.querySelector('.modal h2');
+const modalDescription = document.querySelector('.modal p');
+const modalButton = document.querySelector('.modal button');
+
 const checkButtons = document.querySelectorAll('.actions a.check');
 
 checkButtons.forEach(button => {
-  button.addEventListener('click', event => {
-    button.classList.add('read');
-  });
+  button.addEventListener('click', handleClick);
 });
 
 const deleteButtons = document.querySelectorAll('.actions a.delete');
 
 deleteButtons.forEach(button => {
-  button.addEventListener('click', event => {
-    modal.open();
-  });
+  button.addEventListener('click', event => handleClick(event, false));
 });
+
+function handleClick(event, check = true) {
+  event.preventDefault();
+
+  // Declaração de variáveis
+  const text = check ? 'Marcar como lida' : 'Excluir';
+  const roomId = document.getElementById('room-id').dataset.id;
+  const slug = check ? 'check' : 'delete';
+  const questionId = event.target.dataset.id;
+  const form = document.querySelector('.modal form');
+
+  // Detalhes do Modal
+
+  modalTitle.innerHTML = check ? text : `${text} Pergunta`;
+  modalDescription.innerHTML = `Tem certeza que você deseja ${text.toLowerCase()} ?`;
+  modalButton.innerHTML = `Sim, ${text.toLowerCase()}`;
+  check ? modalButton.classList.remove('red') : modalButton.classList.add('red');
+
+  // Envio de formulário
+  form.setAttribute('action', `/question/${roomId}/${questionId}/${slug}`);
+
+  modal.open();
+}
--- src/server.js
+++ src/server.js
@@ -10,6 +10,9 @@ server.use(express.static('public'));
 
 server.set('views', path.join(__dirname, 'views'));
 
+//configuração de midware, um intermédio entre a rota com as informações e a função a ser executada.
+server.use(express.urlencoded({ extended: true }));
+
 server.use(route);
 
 server.listen(3000, () => console.log('Rodando'));
